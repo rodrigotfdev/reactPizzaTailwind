712171015d550b3af58dc0bbbe1c0e9449050172
Rodrigo <rtf.franca@gmail.com>
2024-07-26 02:30:55 -0300

    feat(layout): build initial app layout

--- src/features/cart/CartOverview.tsx
+++ src/features/cart/CartOverview.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const CartOverview: React.FC = () => {
   return (
@@ -7,7 +8,7 @@ const CartOverview: React.FC = () => {
         <span>23 pizzas</span>
         <span>$23.45</span>
       </p>
-      <a href="#">Open cart &rarr;</a>
+      <Link to='/cart'>Open cart &rarr;</Link>
     </div>
   );
 }
